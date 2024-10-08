5609dd2f73a27c264063a2a7fd06c6a7782dc994
Swapnil Sharma Reveal <swapnil.sharma@revealhealthtech.com>
2024-10-08 11:43:15 +0530

    added superset

---
+++ src/lib/envConfig.ts
@@ -0,0 +1,8 @@
+export const supersetConfig = {
+    supersetUrl: 'http://localhost:8088',
+    username: 'admin',
+    password: 'admin',
+    guestUsername: 'GuestUser',
+    guestFirstName: 'Guest',
+    guestLastName: 'User',
+  };
