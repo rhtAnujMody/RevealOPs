010d05afb71f3a9571c1d86d9aa1814490e86cb8
Swapnil Sharma Reveal <swapnil.sharma@revealhealthtech.com>
2024-10-23 17:27:03 +0530

    Fixed avialble bandwidth dropdown

--- src/screens/EmployeeManagement.tsx
+++ src/screens/EmployeeManagement.tsx
@@ -89,6 +89,7 @@ export default function EmployeeManagement() {
     if (availableBandwidth == '25') options.push({ label: '25%', value: '25' });
     if (availableBandwidth == '50') options.push({ label: '25%', value: '25' }, { label: '50%', value: '50' });
     if (availableBandwidth == '75') options.push({ label: '25%', value: '25' }, { label: '50%', value: '50' }, { label: '75%', value: '75' });
+    if (availableBandwidth == '100') options.push({ label: '25%', value: '25' }, { label: '50%', value: '50' }, { label: '75%', value: '75' }, { label: '100%', value: '100' });
     return options;
   };
 
