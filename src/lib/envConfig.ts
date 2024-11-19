a36224f29e517e326ee88a58190cad351e339cad
Swapnil Sharma Reveal <swapnil.sharma@revealhealthtech.com>
2024-11-19 14:58:12 +0530

    Changed superset URL

--- src/lib/envConfig.ts
+++ src/lib/envConfig.ts
@@ -1,5 +1,5 @@
 export const supersetConfig = {
-    supersetUrl: 'https://apache-superset-2057085725.revealhealthtech.com/',
+    supersetUrl: 'https://superset.revealhealthtech.com/',
     dashboardId: 'b4f32e04-a9d9-4079-a0c1-30f0c678c9e9',
     username: 'admin',
     password: 'admin',
